de9aa67a46c086140f2e9a2b19ea771af0083e35
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-04-29 23:31:42 -0300

    feat: eslint configuration and implementation in progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  env: {
+    es2021: true,
+    jest: true,
+    node: true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  ignorePatterns: ['.eslintrc.js', 'dist/**', 'node_modules/**'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['import', '@typescript-eslint', '@typescript-eslint/eslint-plugin'],
+  root: true,
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    'no-param-reassign': 'off',
   },
-  'prettier/prettier': [
-    'error',
-    {
-      enfOfLine: 'auto',
-    },
-  ],
 };
